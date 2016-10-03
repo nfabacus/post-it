86b8eb82345849aece8845b567fe4f24d47fcff6
noby <n.fujioka@abacusmaths.info>
2016-10-03 14:41:56 +0100

    increates upvotes

--- app.js
+++ app.js
@@ -12,4 +12,13 @@ app.controller('MainCtrl', [
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }]);
