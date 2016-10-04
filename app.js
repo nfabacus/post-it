d18fd3c5c79b685fab45a1c8775b9603a9f1fc32
cyberplanner <saleh.gfx@gmail.com>
2016-10-04 11:06:23 +0100

    Getting the Right Post.

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var app = angular.module('postit', []);
+var app = angular.module('postit', ['ui.router']);
+
+app.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
 
 app.controller('MainCtrl', [
   '$scope',
-  function($scope){
+  'posts',
+  function($scope, posts){
     $scope.test = 'Hello World';
 
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -27,3 +34,31 @@ app.controller('MainCtrl', [
       post.upvotes += 1;
     };
   }]);
+
+  app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+      $scope.post = posts.posts[$stateParams.id];
+    }
+  ]);
+
+  app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        })
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
+        });
+      $urlRouterProvider.otherwise('home');
+    }
+  ]);
