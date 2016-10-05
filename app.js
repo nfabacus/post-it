7dbf9803a279fe229269c67cfb4732a1dbf62949
Giancarlo <gfabrizi@gmail.com>
2016-10-05 11:25:13 +0200

    adds ability to add comment

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ app.controller('MainCtrl', [
     'posts',
     function($scope, $stateParams, posts){
       $scope.post = posts.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
     }
   ]);
 
