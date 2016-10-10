70f75ab7dd78c0424d8dd22c7878d23b92e88f1e
Giancarlo <gfabrizi@gmail.com>
2016-10-10 11:52:44 +0200

    adds persistant new posts to database

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -11,6 +11,12 @@ app.factory('posts', ['$http', function($http) {
     });
   };
 
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data) {
+      o.posts.push(data);
+    });
+  };
+
   return o;
 }]);
 
@@ -35,14 +41,9 @@ app.controller('MainCtrl', [
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
       });
       $scope.title = '';
       $scope.link = '';
