44885a12ef2e1ef511c9d3c21f86947790ea2d1c
noby <n.fujioka@abacusmaths.info>
2016-10-15 00:47:10 +0100

    upvote comment working now

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -6,57 +6,39 @@ app.factory('posts', ['$http', function($http) {
   };
 
   o.get = function(id) {
-    return $http({
-      method: 'GET',
-      url: '/posts/' + id
-    }).then(function(res){
+    return $http.get('/posts/' + id).then(function(res){
       return res.data;
     });
   };
 
   o.getAll = function() {
-    return $http({
-      method: 'GET',
-      url: '/posts'
-    }).success(function(data){
+    return $http.get('/posts').success(function(data){
       angular.copy(data, o.posts);
     });
   };
 
   o.create = function(post) {
-    return $http({
-      method: 'POST',
-      url: '/posts',
-      data: {post: 'post'}
-    }).success(function(data) {
+    return $http.post('/posts', post).success(function(data) {
       o.posts.push(data);
     });
   };
 
   o.upvote = function(post) {
-  return $http({
-    method: 'PUT',
-    url: '/posts/' + post._id + '/upvote'
-  })
+  return $http.put('/posts/' + post._id + '/upvote')
     .success(function(data){
       post.upvotes += 1;
     });
   };
 
   o.addComment = function(id, comment) {
-    return $http({
-      method:'POST',
-      url: '/posts/' + id + '/comments',
-      data: {comment: 'comment'}
-    });
+    return $http.post('/posts/' + id + '/comments', comment);
   };
 
   o.upvoteComment = function(post, comment) {
     return $http({
       method: 'PUT',
       url: '/posts/' + post._id + '/comments/'+ comment._id + '/upvote'
-    })
-      .success(function(data){
+    }).success(function(data){
         comment.upvotes += 1;
       });
   };
