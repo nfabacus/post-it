1cff8cf194e88014190066edd1ef9f162b3134b9
Nobuyuki Fujioka <nobuyuki.fujioka@ecotricity.co.uk>
2017-01-20 08:28:18 +0000

    sets up Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -25,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
@@ -33,6 +36,9 @@ app.use('/users', users);
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+
 mongoose.connect('mongodb://localhost/news');
 
 // catch 404 and forward to error handler
