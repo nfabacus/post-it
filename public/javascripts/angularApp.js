729f7676abab620a4f71a2bb59baa7367e4d10e4
noby <n.fujioka@abacusmaths.info>
2017-01-23 00:27:04 +0000

    adding auth to angularJs

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,5 +1,57 @@
 var app = angular.module('postit', ['ui.router']);
 
+app.factory('auth', ['$http', '$window', function($http, $window) {
+  var auth = {};
+
+  auth.saveToken = function(token) {
+    $window.localStorage['postit-token'] = token;
+  };
+
+  auth.getToken = function(){
+    return $window.localStorage['postit-token'];
+  };
+
+  auth.isLoggedIn = function(){
+    var token = auth.getToken();
+
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function(){
+  $window.localStorage.removeItem('postit-token');
+};
+
+
+  return auth;
+}]);
+
 app.factory('posts', ['$http', function($http) {
   var o = {
     posts: []
